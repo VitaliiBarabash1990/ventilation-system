9eb6ee7f52d2c50b1059c660f401c75c143e17da
vitalii-pc <alievruslan1818@gmail.com>
2025-01-29 18:09:56 +0100

    add_benefits

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,5 +1,29 @@
+import s from "./Benefits.module.css";
+import benefits from "./benefits.json";
+
 const Benefits = () => {
-	return <div>Benefits</div>;
+	return (
+		<div className={`section ${s.Benefits__section}`}>
+			<div className="container">
+				<div className={s.Benefits__section_wraper}>
+					<h2 className={s.Benefits__section_title}>Наші Переваги</h2>
+					<ul className={s.Benefits__section_list}>
+						{benefits.map((benefit, index) => (
+							<li key={index} className={s.Benefits__section_item}>
+								<svg className={s.Benefits__section_item_icon}>
+									<use href={benefit.img}></use>
+								</svg>
+								<h3 className={s.Benefits__section_item_title}>
+									{benefit.title}
+								</h3>
+								<p className={s.Benefits__section_item_text}>{benefit.text}</p>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Benefits;
--- src/components/OurWorks/OurWorks.tsx
+++ src/components/OurWorks/OurWorks.tsx
@@ -1,5 +1,11 @@
+import s from "./OurWorks.module.css";
+
 const OurWorks = () => {
-	return <div>OurWorks</div>;
+	return (
+		<div className={`section ${s.OurWorks__section}`}>
+			<div className="container"></div>
+		</div>
+	);
 };
 
 export default OurWorks;
